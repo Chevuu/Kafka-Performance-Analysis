39366d54babc9fdcf38b9422e2099413ec429d11
ist1108819 <sylvain.migeon@tecnico.ulisboa.pt>
2023-10-12 02:17:26 +0100

    Plots of consumer
    Next step :
    - Automatize parsing & mean calculation
    - Producer case

--- plots/plotScript/plot_mem_percent.py
+++ plots/plotScript/plot_mem_percent.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 # Data from the provided example
 num_consumers = [0, 1, 2, 3, 4, 5]
-mem_percent = [6.87, 6.85, 8.95, 8.95, 11.52, 13.13]
+mem_percent = [6.41219916595488,8.62560920464251,9.99766366879365,11.3886348791639,12.5904888710245,13.5379088579611]
 
 # Plotting memory percent for different numbers of consumers
 plt.bar(num_consumers, mem_percent, color='lightgreen')
